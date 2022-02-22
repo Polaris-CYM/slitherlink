cbf58485d20319a1a98f112749cefc357db392bb
Polaris-CYM <74674480+Polaris-CYM@users.noreply.github.com>
2022-02-22 17:41:08 +0800

    update generate

--- src/generate_problem.py
+++ src/generate_problem.py
@@ -32,6 +32,33 @@ def generate_problem_from_url(url):
     return problem
 
 
+def generate_string_from_url(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    table_element = soup.find('table', id='LoopTable')
+    content = table_element.findAll('tr')  
+    content_list = content[1::2]  # 转化为list。第0行是线，所以跳过；间隔有线，所以每次跳两个。
+    
+    overall_limit = ''
+    nrow = 0
+    for row in content_list:  # 对每一行（有数据的）进行解析
+        nrow += 1
+        col_content = row.findAll('td')
+        col_content_list = col_content[1::2]
+        row_content = ''
+        for col in col_content_list:
+            cell_value = col.string  
+            # cell_value = None if <td align="center"></td>
+            # cell_value = x    if <td align="center">x</td>, where x = 0,1,2,3
+            if not cell_value:  # cell_value == None
+                cell_value = ' '  # 用*符号去表示无提示的数（不能用0来表示，因为0具有实际意义）
+            row_content += cell_value
+        overall_limit += row_content
+    
+    ncol = int(len(overall_limit) / nrow)
+    return nrow, ncol, [overall_limit[i*ncol:i*ncol+ncol] for i in range(nrow)]
+
+
 if __name__ == "__main__":
     problem = generate_problem_from_url(url='http://www.puzzle-loop.com/?v=0&size=5')
     problem.print_problem()
