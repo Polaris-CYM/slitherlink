cf4849508c91e547db54d94438023a4e4bb90b48
Polaris-CYM <74674480+Polaris-CYM@users.noreply.github.com>
2022-03-20 16:36:23 +0800

    add main run

---
+++ src/main.py
@@ -0,0 +1,43 @@
+import time
+from generate_problem import *
+from backtracking import backtracking_solve
+from backjumping import backjumping_solve
+from constraint_propagation_forward import constraint_propagation_forward
+from sat_solve4 import sat_solve
+from minizinc_solve import minizinc_solve
+
+if __name__ == '__main__':
+    alltime = []
+    for _ in range(10):
+        problem = generate_problem_from_url(url='http://www.puzzle-loop.com/?v=0&size=0')
+
+        start_time = time.time()
+        backtracking_solve(problem)
+        bt_time = time.time() - start_time
+
+        start_time = time.time()
+        backjumping_solve(problem)
+        bj_time = time.time() - start_time
+
+        start_time = time.time()
+        constraint_propagation_forward(problem)
+        cf_time = time.time() - start_time
+        
+        start_time = time.time()
+        sat_solve(problem)
+        sat_time = time.time() - start_time
+
+        start_time = time.time()
+        minizinc_solve(problem, "gecode")
+        ge_time = time.time() - start_time
+
+        start_time = time.time()
+        minizinc_solve(problem, "chuffed")
+        chuf_time = time.time() - start_time
+
+        alltime.append([bt_time, bj_time, cf_time, sat_time, ge_time, chuf_time])
+
+        print("="*50)
+        print(alltime)
+        print("="*50)
+
